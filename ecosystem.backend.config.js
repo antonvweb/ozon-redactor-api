4c412bd35b777a76bf690ba8ab80cd5006b0d9e7
Anton Volkov <66030600+AHTOHFANTOM@users.noreply.github.com>
2025-12-06 11:40:16 +0300

    добавил деплой

---
+++ ecosystem.backend.config.js
@@ -0,0 +1,63 @@
+module.exports = {
+  apps: [
+    {
+      name: "auth-service",
+      script: "java",
+      args: ["-jar", "authService-1.0.0.jar"],
+      cwd: "/var/www/ozon-redactor/backend",
+      exec_mode: "fork",
+      instances: 1,
+      autorestart: true,
+      max_memory_restart: "1G",
+      env: {
+        SPRING_PROFILES_ACTIVE: "prod",
+        TZ: "Europe/Moscow"
+      }
+    },
+
+    {
+      name: "ozon-api",
+      script: "java",
+      args: ["-jar", "ozonApi-1.0.0.jar"],
+      cwd: "/var/www/ozon-redactor/backend",
+      exec_mode: "fork",
+      instances: 1,
+      autorestart: true,
+      max_memory_restart: "1G",
+      env: {
+        SPRING_PROFILES_ACTIVE: "prod",
+        TZ: "Europe/Moscow"
+      }
+    },
+
+    {
+      name: "user-service",
+      script: "java",
+      args: ["-jar", "userService-1.0.0.jar"],
+      cwd: "/var/www/ozon-redactor/backend",
+      exec_mode: "fork",
+      instances: 1,
+      autorestart: true,
+      max_memory_restart: "1G",
+      env: {
+        SPRING_PROFILES_ACTIVE: "prod",
+        TZ: "Europe/Moscow"
+      }
+    },
+
+    {
+      name: "notification-service",
+      script: "java",
+      args: ["-jar", "notification-1.0.0.jar"],
+      cwd: "/var/www/ozon-redactor/backend",
+      exec_mode: "fork",
+      instances: 1,
+      autorestart: true,
+      max_memory_restart: "512M",
+      env: {
+        SPRING_PROFILES_ACTIVE: "prod",
+        TZ: "Europe/Moscow"
+      }
+    }
+  ]
+};
