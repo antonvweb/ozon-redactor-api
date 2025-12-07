2b05fa6a0186eda6b927df2142c13c45963640b7
Anton Volkov <66030600+AHTOHFANTOM@users.noreply.github.com>
2025-12-07 11:14:07 +0300

    изменения

--- userService/src/main/java/org/ozonLabel/user/UserServiceApplication.java
+++ userService/src/main/java/org/ozonLabel/user/UserServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"org.ozonLabel.user", "org.ozonLabel.common"})
 @EnableJpaRepositories(basePackages = {
         "org.ozonLabel.user.repository",
         "org.ozonLabel.ozonApi.repository",
