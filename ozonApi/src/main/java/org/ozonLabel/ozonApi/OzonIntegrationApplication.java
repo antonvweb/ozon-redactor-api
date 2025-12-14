65c8ef48322047680b2eabdf95345105f3fa829a
Anton Volkov <66030600+AHTOHFANTOM@users.noreply.github.com>
2025-12-14 21:51:35 +0300

    ОГРОМНЫЕ ИЗМЕНЕНИЯ

--- ozonApi/src/main/java/org/ozonLabel/ozonApi/OzonIntegrationApplication.java
+++ ozonApi/src/main/java/org/ozonLabel/ozonApi/OzonIntegrationApplication.java
@@ -1,12 +1,26 @@
 package org.ozonLabel.ozonApi;
 
+import org.ozonLabel.common.exception.user.GlobalExceptionHandler; // бин, который конфликтует
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
-@SpringBootApplication(scanBasePackages = {"org.ozonLabel.common"})
+@SpringBootApplication
+@ComponentScan(
+        basePackages = {
+                "org.ozonLabel.ozonApi",   // пакет твоего сервиса
+                "org.ozonLabel.common"     // весь common
+        },
+        excludeFilters = @ComponentScan.Filter(
+                type = FilterType.ASSIGNABLE_TYPE,
+                classes = GlobalExceptionHandler.class // исключаем только конфликтный бин
+        )
+)
 public class OzonIntegrationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OzonIntegrationApplication.class, args);
     }
+
 }
--- userService/src/main/java/org/ozonLabel/user/UserServiceApplication.java
+++ userService/src/main/java/org/ozonLabel/user/UserServiceApplication.java
@@ -1,11 +1,26 @@
 package org.ozonLabel.user;
 
+import org.ozonLabel.common.exception.ozon.GlobalExceptionHandler; // бин из ozon, который конфликтует
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
-@SpringBootApplication(scanBasePackages = {"org.ozonLabel.common"})
+@SpringBootApplication
+@ComponentScan(
+        basePackages = {
+                "org.ozonLabel.user",   // пакет твоего сервиса
+                "org.ozonLabel.common"  // весь common
+        },
+        excludeFilters = @ComponentScan.Filter(
+                type = FilterType.ASSIGNABLE_TYPE,
+                classes = GlobalExceptionHandler.class // исключаем конфликтный бин из ozon
+        )
+)
 public class UserServiceApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
     }
+
 }
