146745151d6c9d7df2ea8014eb6b68f00d5509e3
Anton Volkov <66030600+AHTOHFANTOM@users.noreply.github.com>
2026-02-20 15:22:43 +0300

    fix

--- common/src/main/java/org/ozonLabel/common/dto/label/ElementDto.java
+++ common/src/main/java/org/ozonLabel/common/dto/label/ElementDto.java
@@ -14,6 +14,7 @@ import java.math.BigDecimal;
 public class ElementDto {
     // Обязательные поля
     private String id;
+    private String name;
     private String type;
     private Integer layerId;
     private BigDecimal x;
